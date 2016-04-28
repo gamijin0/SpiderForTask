c193c266747508a5620597b86bd9154eea81f3b8
RealmL <872138492@qq.com>
2016-04-29 00:08:32 +0800

    change softwarepatent

--- SoftwarePatent/SoftwarePatent.py
+++ SoftwarePatent/SoftwarePatent.py
@@ -3,8 +3,7 @@ __author__ = 'RealmL'
 
 import csv
 import requests
-from bs4 import  BeautifulSoup
-import sys
+from bs4 import BeautifulSoup
 import json
 
 try:
@@ -16,30 +15,59 @@ except ImportError:
         from io import StringIO
 
 
-def get_html(company):
-
+def get_ResultList(company):
     url='http://www.chinacopyright.org.cn/findsoftjosn.aspx'
-    data = {
-            'softcopy':company
-        }
-
+    data={
+        'softcopy':company
+    }
     JsonRes = requests.post(url=url,data=data)
     res = json.loads(JsonRes.text)
-    print(res)
-
+    #print(res)
+    result_list=[]
     for i in res:
-        print(i)
+        #print(i)
+        if i==None:
+            result_list.append('没有找到相关的证书资料')
+        else:
+            sub_list=[]
+            for value in i.values():
+                #print(value)
+                sub_list.append(value)
+            result_list.append(sub_list)
+
+    return result_list
 
 
+# def get_patent(html):
+#     bs=BeautifulSoup(html.text,'html.parser')
+#     result_list=[]
+#     for tr in bs.find('form',{'method':'post','id':'form1'}).find('div',{'id':'list'}).find('tbody').find_all('tr'):
+#         sub_list=[]
+#         for td in tr.find_all('td'):
+#             sub_list.append(td.string)
+#         sub_list.append(sub_list)
+#     return result_list
+
+def read_csv(name):
+    reader = csv.reader(open(name, 'r'))
+    company_list=[]
+    for line in reader:
+        company_list.append(line)
+
+    return company_list
+
+# def write_csv(data,name):
+#     file_name = name
+#     with open(file_name, 'a', errors='ignore', newline='') as f:
+#         f_csv = csv.writer(f)
+#         f_csv.writerows(data)
 
 
-
-if __name__ == '__main__':
-    html=get_html('百度')#虽然不用搜百度，主要是我随便试了几个他给的公司，都没有证书，有的不多
-    #print(html.text)
-    #result_list=get_patent(html)
-    #print(result_list)
-
-    # result_list.insert(0,('登记号','分类号','软件全称','软件简称','版本号','著作权人','批准日期'))
-    # write_csv(result_list,r'res.csv')
+if __name__ =="__main__":
+    company_list=read_csv('li.csv')
+    print(company_list)
+    # for company in company_list:
+    #     result_list=get_ResultList(company)
+    #     #print(result_list)
+    #     write_csv(result_list,'res.csv')
 
