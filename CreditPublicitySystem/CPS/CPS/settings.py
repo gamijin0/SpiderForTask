b03e1817f5459ee7e5c454a77130bab7a8325795
chaos <497386856@qq.com>
2016-07-19 16:12:54 +0800

    scrapy-CreditPS-v2.0 -completed

--- CreditPublicitySystem/CPS/CPS/settings.py
+++ CreditPublicitySystem/CPS/CPS/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'CPS.spiders'
 #USER_AGENT = 'CPS (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-# ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -72,9 +72,9 @@ ITEM_PIPELINES = {
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 0.5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 20
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
